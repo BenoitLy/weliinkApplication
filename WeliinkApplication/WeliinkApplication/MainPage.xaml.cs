7be564a493a7c0b02287e3e1d5b4e8e9128fb70e
Pafy78 <pierre.baudoin@devinci.fr>
2018-01-16 21:21:20 +0100

    test post request

--- WeliinkApplication/WeliinkApplication/MainPage.xaml.cs
+++ WeliinkApplication/WeliinkApplication/MainPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -11,7 +13,39 @@ namespace WeliinkApplication
 	{
 		public MainPage()
 		{
-			InitializeComponent();
-		}
-	}
+            InitializeComponent();
+
+            try
+            {
+                string url = "http://weliink.fr/post/api.php";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET/POST";
+                //using GET - request.Headers.Add ("Authorization","Authorizaation value");
+                request.ContentType = "application/json";
+                HttpWebResponse myResp = (HttpWebResponse)request.GetResponse();
+                string responseText;
+
+                using (var response = request.GetResponse())
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseText = reader.ReadToEnd();
+                        //Console.WriteLine(responseText);
+                        Name.Text = responseText;
+                    }
+                }
+            }
+
+            catch (WebException exception)
+            {
+                string responseText;
+                using (var reader = new StreamReader(exception.Response.GetResponseStream()))
+                {
+                    responseText = reader.ReadToEnd();
+                    Console.WriteLine(responseText);
+                    Name.Text = responseText;
+                }
+            }
+        }
+    }
 }
--- WeliinkApplication/WeliinkApplication/obj/Debug/netstandard2.0/WeliinkApplication.MainPage.xaml.g.cs
+++ WeliinkApplication/WeliinkApplication/obj/Debug/netstandard2.0/WeliinkApplication.MainPage.xaml.g.cs
@@ -16,8 +16,12 @@ namespace WeliinkApplication {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label Name;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            Name = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "Name");
         }
     }
 }
